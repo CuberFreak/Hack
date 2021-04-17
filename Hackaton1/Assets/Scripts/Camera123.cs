a57b70a2914fbdee271a0d4d2f9c092b05ed9cb3
CuberFreak <mmmmmm000000202020@gmail.com>
2021-04-17 23:49:44 +0300

    game2

--- Hackaton1/Assets/Scripts/Camera123.cs
+++ Hackaton1/Assets/Scripts/Camera123.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camera123 : MonoBehaviour
 {
     public GameObject Player;
 
